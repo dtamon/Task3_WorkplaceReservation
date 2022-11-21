d91122aa6915290e964496d47602792ee5b14dd1
dtamon <dominiktamon@gmail.com>
2022-11-21 08:23:32 +0100

    Add project files.

---
+++ Task3_WorkplaceReservation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Task3_WorkplaceReservation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
